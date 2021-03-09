22a490e281f2624153f81aec4ca9ef0c48846105
zelytra <alexbreuillet@gmail.com>
2021-03-09 19:54:06 +0100

    Initial commit

---
+++ src/main/java/fr/zelytra/daedalus/Daedalus.java
@@ -0,0 +1,16 @@
+package fr.zelytra.daedalus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Daedalus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Start");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
