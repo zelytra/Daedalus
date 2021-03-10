070161f55c778b097e4be534b7cf1a3be5ed070a
zelytra <alexbreuillet@gmail.com>
2021-03-10 19:06:40 +0100

    Adding grid generator

--- src/main/java/fr/zelytra/daedalus/maze/Maze.java
+++ src/main/java/fr/zelytra/daedalus/maze/Maze.java
@@ -1,9 +1,16 @@
 package fr.zelytra.daedalus.maze;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;import org.bukkit.entity.Player;
+
+
+
 public class Maze {
     private int[][] maze;
-    private final int size;
+    private int size;
     private final boolean complexity;
+    private Player player;
 
     /**
      * Maze object
@@ -17,8 +24,20 @@ public class Maze {
         this.maze = new int[this.size][this.size];
     }
 
-    private void generate() {
+    public Maze(int size, boolean complexity, Player player) {
+        this.size = size;
+        this.complexity = complexity;
+        this.maze = new int[this.size][this.size];
+        this.player = player;
+    }
+
+    private void generateGrid() {
         //Fill outline with wall
+        // Wall = 1| void = 0
+        logPlayer(this.player,"§6§lGenerated grid...");
+        if (this.size % 2 != 0) {
+            this.size--;
+        }
         int nbr = 2;
         int[] wall = new int[this.size];
         int[] line = new int[this.size];
@@ -32,33 +51,52 @@ public class Maze {
             }
         }
         for (int x = 0; x < this.size; x++) {
-            if (x % 2 == 0) {
-                for (int z = 1; z < this.size-1; z++) {
-                    this.maze[x][z]=line[z];
-                }
-            }
-            else{
+            this.maze[x][0] = 1;
+            if (!(x % 2 == 0)) {
                 for (int z = 0; z < this.size; z++) {
-                    this.maze[x][z]=1;
+                    this.maze[z][x] = line[z];
+                }
+            } else {
+                for (int z = 0; z < this.size; z++) {
+                    this.maze[z][x] = 1;
                 }
             }
         }
-        //Fill case value
+        //Fill case with value
         for (int x = 0; x < this.size; x++) {
             for (int z = 0; z < this.size; z++) {
-
+                if (this.maze[x][z] == 0) {
+                    this.maze[x][z] = nbr;
+                    nbr++;
+                }
             }
         }
-
-
     }
 
-    public int[][] getMaze(){
-        generate();
+    private void generateWay() {
+        boolean isGenerated = false;
+        logPlayer(this.player,"§6§lGenerated way...");
+        while (!isGenerated) {
+            int x = (int) Math.random() % (this.size - 2) + 1;
+
+            isGenerated =true;
+        }
+    }
+
+    public int[][] getMaze() {
+        generateGrid();
+        generateWay();
         return this.maze;
     }
 
     public int getSize() {
         return this.size;
+    }
+
+    private void logPlayer(Player player,String msg){
+        if(player!=null){
+            BaseComponent txt = new TextComponent(msg);
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR,txt);
+        }
     }
 }
--- src/main/java/fr/zelytra/daedalus/maze/MazeCommands.java
+++ src/main/java/fr/zelytra/daedalus/maze/MazeCommands.java
@@ -4,11 +4,13 @@ import fr.zelytra.daedalus.Daedalus;
 import fr.zelytra.daedalus.utils.Message;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
 import org.bukkit.entity.Player;
 
 public class MazeCommands implements CommandExecutor {
@@ -22,26 +24,23 @@ public class MazeCommands implements CommandExecutor {
         if (args.length == 2 && args[0].equalsIgnoreCase("generate")) {
 
             player.sendMessage(Message.getPlayerPrefixe() + " §cStarting generation...");
-            Maze maze = new Maze(Integer.parseInt(args[1]), true);
+            Maze maze = new Maze(Integer.parseInt(args[1]), true, player);
             int[][] m = maze.getMaze();
             Location location = player.getLocation();
 
-            Bukkit.getScheduler().runTaskAsynchronously(Daedalus.plugin, new Runnable() {
-                @Override
-                public void run() {
-                    for (int x = 0; x < maze.getSize(); x++) {
-                        for (int z = 0; z < maze.getSize(); z++) {
-                            location.setX(player.getLocation().getX()+x);
-                            location.setZ(player.getLocation().getZ()+z);
-                            if (m[x][z] == 1) {
-                                location.getBlock().setType(Material.BLACK_CONCRETE);
-                            } else {
-                                location.getBlock().setType(Material.WHITE_CONCRETE);
-                            }
-                        }
+
+            for (int x = 0; x < maze.getSize(); x++) {
+                for (int z = 0; z < maze.getSize(); z++) {
+                    location.setX(player.getLocation().getX() + x);
+                    location.setZ(player.getLocation().getZ() + z);
+                    if (m[x][z] == 1) {
+                        location.getBlock().setType(Material.BLACK_CONCRETE);
+                    } else {
+                        location.getBlock().setType(Material.WHITE_CONCRETE);
                     }
                 }
-            });
+            }
+
             return true;
         }
 
@@ -50,13 +49,13 @@ public class MazeCommands implements CommandExecutor {
 
     private static void setBlockInNativeChunkSection(World world, int x, int y, int z, int blockId, byte data) {
 
-        net.minecraft.server.v1_14_R1.World nmsWorld = ((CraftWorld) world).getHandle();
-        net.minecraft.server.v1_14_R1.Chunk nmsChunk = nmsWorld.getChunkAt(x >> 4, z >> 4);
-        IBlockData ibd = net.minecraft.server.v1_14_R1.Block.getByCombinedId(blockId + (data << 12));
+        net.minecraft.server.v1_16_R3.World nmsWorld = ((CraftWorld) world).getHandle();
+        net.minecraft.server.v1_16_R3.Chunk nmsChunk = nmsWorld.getChunkAt(x >> 4, z >> 4);
+        net.minecraft.server.v1_16_R3.IBlockData ibd = net.minecraft.server.v1_16_R3.Block.getByCombinedId(blockId + (data << 12));
 
-        ChunkSection cs = nmsChunk.getSections()[y >> 4];
+        net.minecraft.server.v1_16_R3.ChunkSection cs = nmsChunk.getSections()[y >> 4];
         if (cs == nmsChunk.a()) {
-            cs = new ChunkSection(y >> 4 << 4);
+            cs = new net.minecraft.server.v1_16_R3.ChunkSection(y >> 4 << 4);
             nmsChunk.getSections()[y >> 4] = cs;
         }
         cs.setType(x & 15, y & 15, z & 15, ibd);
