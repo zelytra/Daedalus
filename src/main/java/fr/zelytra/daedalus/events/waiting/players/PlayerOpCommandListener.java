e51b9efac8363d87f22d8a0cedc95894033adc3b
punisherFive <punishercinq@gmail.com>
2021-05-03 17:16:03 +0200

    deop cmd

--- src/main/java/fr/zelytra/daedalus/events/waiting/players/PlayerOpCommandListener.java
+++ src/main/java/fr/zelytra/daedalus/events/waiting/players/PlayerOpCommandListener.java
@@ -25,6 +25,17 @@ public class PlayerOpCommandListener implements Listener {
             }
         }
 
+        if(e.getMessage().startsWith("/deop") && e.getMessage().split(" ").length == 2){
+
+            String pn = e.getMessage().split(" ")[1];
+
+            if(Bukkit.getPlayer(pn) != null){
+                Bukkit.getPlayer(pn).getInventory().clear();
+                Bukkit.getPlayer(pn).getInventory().setItem(4, new ItemBuilder(Material.WHITE_BANNER, "§7Team selection").getItemStack());
+            }
+
+        }
+
     }
 
     @EventHandler
@@ -39,6 +50,17 @@ public class PlayerOpCommandListener implements Listener {
                 Bukkit.getPlayer(pn).getInventory().setItem(8, new ItemBuilder(Material.WHITE_BANNER, "§7Team selection").getItemStack());
                 Bukkit.getPlayer(pn).getInventory().setItem(0, new ItemBuilder(Material.COMPARATOR, "§7Game settings").getSettings());
                 Bukkit.getPlayer(pn).getInventory().setItem(4, new ItemBuilder(Material.BELL, "§6Start game", "§7Click here to start your game with the actual configuration").getItemStack());
+            }
+
+        }
+
+        if(e.getCommand().startsWith("/deop") && e.getCommand().split(" ").length == 2){
+
+            String pn = e.getCommand().split(" ")[1];
+
+            if(Bukkit.getPlayer(pn) != null){
+                Bukkit.getPlayer(pn).getInventory().clear();
+                Bukkit.getPlayer(pn).getInventory().setItem(4, new ItemBuilder(Material.WHITE_BANNER, "§7Team selection").getItemStack());
             }
 
         }
