925751aa9f7b2794e957c47cee8ebf155e814444
Zelytra <alexbreuillet@gmail.com>
2021-06-03 18:15:57 +0200

    Add server icon

--- src/main/java/fr/zelytra/daedalus/events/ServerPingListener.java
+++ src/main/java/fr/zelytra/daedalus/events/ServerPingListener.java
@@ -1,13 +1,27 @@
 package fr.zelytra.daedalus.events;
 
 import fr.zelytra.daedalus.Daedalus;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+
 public class ServerPingListener implements Listener {
     @EventHandler
     public void onPing(ServerListPingEvent e) {
+
+        try {
+            e.setServerIcon(Bukkit.loadServerIcon(getServerIcon()));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+
         if (Daedalus.getInstance().getGameManager().isStarted()) {
             e.setMotd("§6| §2Maze-Game §6ᒥ §8<< §6Daedalus §8>> §6ᒧ §2daedalus.§azelytra.fr     §6| §7Game Status §6: §fMaze Generation in progress...");
             return;
@@ -24,6 +38,19 @@ public class ServerPingListener implements Listener {
                 break;
         }
 
+
+    }
+
+    private File getServerIcon() {
+        try {
+            URL url = new URL("https://github.com/zelytra/Daedalus/blob/master/img/server-icon.png");
+            BufferedImage img = ImageIO.read(url);
+            return new File("server-icon.png");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
 
     }
 }
