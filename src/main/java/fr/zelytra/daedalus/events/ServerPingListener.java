a596b44a05252164af5c43e26d872962ebfd610d
Zelytra <alexbreuillet@gmail.com>
2021-06-03 15:02:49 +0200

    Motd update v2

--- src/main/java/fr/zelytra/daedalus/events/ServerPingListener.java
+++ src/main/java/fr/zelytra/daedalus/events/ServerPingListener.java
@@ -1,12 +1,29 @@
 package fr.zelytra.daedalus.events;
 
+import fr.zelytra.daedalus.Daedalus;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
 public class ServerPingListener implements Listener {
     @EventHandler
-    public void onPing(ServerListPingEvent e){
-        e.setMotd("§8[§6Maze-Minigame§8]   §8<< §7< §6Daedalus §7> §8>>   §8[§61.16.4/5§8]§r\n             §8<< §7<- §6daedalus.zelytra.fr §7-> §8>>");
+    public void onPing(ServerListPingEvent e) {
+        if (Daedalus.getInstance().getGameManager().isStarted()) {
+            e.setMotd("§6| §2Maze-Game §6ᒥ §8<< §6Daedalus §8>> §6ᒧ §2daedalus.§azelytra.fr     §6| §7Game Status §6: §fMaze Generation in progress...");
+            return;
+        }
+        switch (Daedalus.getInstance().getGameManager().getState()) {
+            case WAIT:
+                e.setMotd("§6| §2Maze-Game §6ᒥ §8<< §6Daedalus §8>> §6ᒧ §2daedalus.§azelytra.fr     §6| §7Game Status §6: §fWaiting for players...");
+                break;
+            case RUNNING:
+                e.setMotd("§6| §2Maze-Game §6ᒥ §8<< §6Daedalus §8>> §6ᒧ §2daedalus.§azelytra.fr     §6| §7Game Status §6: §fGame running ! §b§kON");
+                break;
+            case FINISHED:
+                e.setMotd("§6| §2Maze-Game §6ᒥ §8<< §6Daedalus §8>> §6ᒧ §2daedalus.§azelytra.fr     §6| §7Game Status §6: §fWaiting for §cRestart §f!");
+                break;
+        }
+
+
     }
 }
