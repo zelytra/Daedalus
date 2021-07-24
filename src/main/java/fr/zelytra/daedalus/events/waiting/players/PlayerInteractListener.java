cd846950cc5a2ee53af01f51258e63ff1dc46c6e
zelytra <alexbreuillet@gmail.com>
2021-07-24 14:41:07 +0200

    Ticket #115

---
+++ src/main/java/fr/zelytra/daedalus/events/waiting/players/PlayerInteractListener.java
@@ -0,0 +1,16 @@
+package fr.zelytra.daedalus.events.waiting.players;
+
+import fr.zelytra.daedalus.Daedalus;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class PlayerInteractListener implements Listener {
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e) {
+
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK && Daedalus.getInstance().getGameManager().isWaiting())
+            e.setCancelled(true);
+    }
+}
