f8b4a70198d5eabd5d0455615f23e01bee83d2fd
zelytra <alexbreuillet@gmail.com>
2021-08-24 10:42:42 +0200

    Ticket #156

--- src/main/java/fr/zelytra/daedalus/commands/revive/HadesRevive.java
+++ src/main/java/fr/zelytra/daedalus/commands/revive/HadesRevive.java
@@ -6,14 +6,20 @@ import fr.zelytra.daedalus.managers.faction.PlayerStatus;
 import fr.zelytra.daedalus.managers.game.settings.GameSettings;
 import fr.zelytra.daedalus.managers.gods.GodsEnum;
 import fr.zelytra.daedalus.utils.Message;
+import fr.zelytra.daedalus.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
 public class HadesRevive implements CommandExecutor {
@@ -91,6 +97,23 @@ public class HadesRevive implements CommandExecutor {
         if (GodsEnum.HADES.getGod().teamEffects() != null) {
             player.addPotionEffects(GodsEnum.HADES.getGod().teamEffects());
         }
+
+        player.getInventory().setHelmet(Utils.EnchantedItemStack(Material.DIAMOND_HELMET, Enchantment.PROTECTION_ENVIRONMENTAL,2));
+        player.getInventory().setChestplate(Utils.EnchantedItemStack(Material.DIAMOND_CHESTPLATE, Enchantment.PROTECTION_ENVIRONMENTAL,2));
+        player.getInventory().setLeggings(Utils.EnchantedItemStack(Material.IRON_LEGGINGS, Enchantment.PROTECTION_ENVIRONMENTAL,2));
+        player.getInventory().setBoots(Utils.EnchantedItemStack(Material.IRON_BOOTS, Enchantment.PROTECTION_ENVIRONMENTAL,2));
+
+        player.getInventory().setItemInMainHand(Utils.EnchantedItemStack(Material.DIAMOND_SWORD, Enchantment.DAMAGE_ALL,2));
+        player.getInventory().setItemInOffHand(new ItemStack(Material.SHIELD));
+
+
+        player.getInventory().addItem(Utils.EnchantedItemStack(Material.IRON_PICKAXE, Enchantment.DIG_SPEED,2));
+        player.getInventory().addItem(new ItemStack(Material.COOKED_BEEF,20));
+        player.getInventory().addItem(new ItemStack(Material.BOW));
+        player.getInventory().addItem(new ItemStack(Material.ARROW,16));
+
+        player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE,100,9,true,true,true));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS,100,9,true,true,true));
 
     }
 }
