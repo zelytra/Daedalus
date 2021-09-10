182fd4fe8d7c66cf94752b0494134f26fec16fcc
Zelytra <alexbreuillet@gmail.com>
2021-09-10 11:35:54 +0200

    Fis pause listener actions

--- src/main/java/fr/zelytra/daedalus/commands/pause/PauseListener.java
+++ src/main/java/fr/zelytra/daedalus/commands/pause/PauseListener.java
@@ -5,7 +5,12 @@ import fr.zelytra.daedalus.managers.game.settings.GameSettings;
 import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PauseListener implements Listener {
@@ -24,6 +29,36 @@ public class PauseListener implements Listener {
 
     @EventHandler
     public void onDamage(EntityDamageByEntityEvent e) {
+        if (Daedalus.getInstance().getGameManager().getTimeManager().isPause())
+            e.setCancelled(true);
+    }
+    @EventHandler
+    public void placeBlock(BlockPlaceEvent e) {
+        if (Daedalus.getInstance().getGameManager().getTimeManager().isPause())
+            e.setCancelled(true);
+
+    }
+
+    @EventHandler
+    public void breakBlock(BlockBreakEvent e) {
+        if (Daedalus.getInstance().getGameManager().getTimeManager().isPause())
+            e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void interact(PlayerInteractEvent e) {
+        if (Daedalus.getInstance().getGameManager().getTimeManager().isPause())
+            e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void dropItem(PlayerDropItemEvent e) {
+        if (Daedalus.getInstance().getGameManager().getTimeManager().isPause())
+            e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void inventory(InventoryClickEvent e) {
         if (Daedalus.getInstance().getGameManager().getTimeManager().isPause())
             e.setCancelled(true);
     }
