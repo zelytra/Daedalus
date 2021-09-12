88d78720dc904222a4d5ee8c4ae18080c06962aa
zelytra <alexbreuillet@gmail.com>
2021-09-12 22:17:19 +0200

    some debug

--- src/main/java/fr/zelytra/daedalus/events/running/environnement/structure/InteractListener.java
+++ src/main/java/fr/zelytra/daedalus/events/running/environnement/structure/InteractListener.java
@@ -3,6 +3,7 @@ package fr.zelytra.daedalus.events.running.environnement.structure;
 import fr.zelytra.daedalus.Daedalus;
 import fr.zelytra.daedalus.managers.game.GameStatesEnum;
 import fr.zelytra.daedalus.managers.maze.Maze;
+import fr.zelytra.daedalus.managers.maze.Vector2;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -25,10 +26,15 @@ public class InteractListener implements Listener {
         if (maze == null) {
             return;
         }
+        System.out.println("trigger");
         int groundY = (int) maze.getOrigin().getY();
         if (e.getAction() == Action.LEFT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (e.getClickedBlock().getY() >= groundY + (wallHigh - limit))
+            Vector2 matrixCoordinate = new Vector2((int) (e.getClickedBlock().getX() - maze.getOrigin().getX() + 1), (int) (e.getClickedBlock().getZ() - maze.getOrigin().getZ() + 1));
+            System.out.println(maze.getMaze()[matrixCoordinate.x][matrixCoordinate.z]);
+            if (maze.getMaze()[matrixCoordinate.x][matrixCoordinate.z] >= 1 && e.getClickedBlock().getY() >= groundY + (wallHigh - limit)) {
+                System.out.println("cancel");
                 e.setCancelled(true);
+            }
         }
 
     }
--- src/main/java/fr/zelytra/daedalus/events/running/environnement/structure/PortalListener.java
+++ src/main/java/fr/zelytra/daedalus/events/running/environnement/structure/PortalListener.java
@@ -5,6 +5,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerPortalEvent;
 
 public class PortalListener implements Listener {
+
     @EventHandler
     public void onPortal(PlayerPortalEvent e){
         e.setCanCreatePortal(false);
